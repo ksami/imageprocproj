a3efb1326f4011b8d669516b83b32d65071f70e1
ksami <ksami.ihide@gmail.com>
2015-05-25 17:02:51 +0900

    update trans and scale def

--- matlab/findScalingMatrix.m
+++ matlab/findScalingMatrix.m
@@ -1,6 +1,7 @@
-function scaling = findScalingMatrix(img, scaledImg)
-% Given an img and its scaled result such that
-% [scaledImg] = [scaling] * [img]
-% find [scaling]
-% Inputs : [double] img, [double] scaledImg
-% Outputs: [double] scaling
+function scaling = findScalingMatrix(centre_face, centre_scaled)
+% Given centres of facial features, find rotation matrix to
+% transform centre_face into centre_scaled
+% centre_face and centre_scaled are both structures containing
+% eyeL, eyeR, nose, mouth
+% each of the features are points with x y coords
+% eg. centre_face.eyeL.x
--- matlab/findTranslationMatrix.m
+++ matlab/findTranslationMatrix.m
@@ -1,6 +1,7 @@
-function translation = findTranslationMatrix(img, translatedImg)
-% Given an img and its translated result such that
-% [translatedImg] = [translation] * [img]
-% find [translation]
-% Inputs : [double] img, [double] translatedImg
-% Outputs: [double] translation
+function translation = findTranslationMatrix(centre_face, centre_translated)
+% Given centres of facial features, find rotation matrix to
+% transform centre_face into centre_translated
+% centre_face and centre_translated are both structures containing
+% eyeL, eyeR, nose, mouth
+% each of the features are points with x y coords
+% eg. centre_face.eyeL.x
