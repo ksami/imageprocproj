4e4344f618bbf1c5c0981bc52eac211387328e4d
alanxwl <alanxwl@live.com>
2015-05-25 21:33:25 +0900

    Update code

--- matlab/findRotationMatrix.m
+++ matlab/findRotationMatrix.m
@@ -6,4 +6,22 @@ function rotation = findRotationMatrix(centre_face, centre_rotated)
 % each of the features are points with x y coords
 % eg. centre_face.eyeL.x
 
+%offset eyeL (original) and eyeL1 (rotated); nose as origin; express as x y coords
+eyeL = [centre_face.eyeL.x - centre_face.nose.x + 1, centre_face.eyeL.y - centre_face.nose.y + 1];
+eyeL1 = [centre_rotated.eyeL.x - centre_face.nose.x + 1, centre_rotated.eyeL.y - centre_face.nose.y + 1];
+
+%find angle between eyeL (original) and eyeL1 (rotated)
+costheta = (dot(eyeL, eyeL1))/((norm(eyeL))*(norm(eyeL1)));
+theta = acos(costheta);
+
+%check whether angle (theta) is negative
+xaxis = [1,0];
+rho = acos((dot(eyeL, xaxis))/((norm(eyeL))*(norm(xaxis))));
+rho1 = acos((dot(eyeL1, xaxis))/((norm(eyeL1))*(norm(axis))));
+if (rho1>rho)
+    theta = -theta;
+end
+
+%build rotation matrix, where [x'; y'; 1]=[rotation]*[x; y; 1]
+rotation = [cos(theta), -sin(theta), 0; sin(theta), cos(theta), 0; 0, 0, 1];
 end
