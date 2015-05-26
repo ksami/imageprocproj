e8078abe8702389e2d322baa084e85a954e01fc5
alanxwl <alanxwl@live.com>
2015-05-26 16:21:06 +0900

    Update findTranslationMatrix.m

--- matlab/findTranslationMatrix.m
+++ matlab/findTranslationMatrix.m
@@ -12,10 +12,10 @@ Dx3=centre_translated.nose.x-centre_face.nose.x;
 Dx4=centre_translated.mouth.x-centre_face.mouth.x;
 Dx=mean([Dx1,Dx2,Dx3,Dx4]);
 
-Dx1=centre_translated.eyeL.y-centre_face.eyeL.y;
-Dx2=centre_translated.eyeR.y-centre_face.eyeR.y;
-Dx3=centre_translated.nose.y-centre_face.nose.y;
-Dx4=centre_translated.mouth.y-centre_face.mouth.y;
+Dy1=centre_translated.eyeL.y-centre_face.eyeL.y;
+Dy2=centre_translated.eyeR.y-centre_face.eyeR.y;
+Dy3=centre_translated.nose.y-centre_face.nose.y;
+Dy4=centre_translated.mouth.y-centre_face.mouth.y;
 Dy=mean([Dy1,Dy2,Dy3,Dy4]);
 
 translation = [1 0 Dx; 0 1 Dy; 0 0 1];
