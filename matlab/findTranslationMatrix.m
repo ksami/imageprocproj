75b5fa7e0f99dd291c7060bd58c777b51f9aaed5
alanxwl <alanxwl@live.com>
2015-05-27 17:02:55 +0900

    Update findTranslationMatrix.m

--- matlab/findTranslationMatrix.m
+++ matlab/findTranslationMatrix.m
@@ -6,18 +6,18 @@ function translation = findTranslationMatrix(centre_face, centre_translated)
 % each of the features are points with x y coords
 % eg. centre_face.eyeL.x
 
-Dx1=centre_translated.eyeL.x-centre_face.eyeL.x;
-Dx2=centre_translated.eyeR.x-centre_face.eyeR.x;
+%Dx1=centre_translated.eyeL.x-centre_face.eyeL.x;
+%Dx2=centre_translated.eyeR.x-centre_face.eyeR.x;
 Dx3=centre_translated.nose.x-centre_face.nose.x;
-Dx4=centre_translated.mouth.x-centre_face.mouth.x;
-Dx=mean([Dx1,Dx2,Dx3,Dx4]);
+%Dx4=centre_translated.mouth.x-centre_face.mouth.x;
+%Dx=mean([Dx1,Dx2,Dx3,Dx4]);
 
-Dy1=centre_translated.eyeL.y-centre_face.eyeL.y;
-Dy2=centre_translated.eyeR.y-centre_face.eyeR.y;
+%Dy1=centre_translated.eyeL.y-centre_face.eyeL.y;
+%Dy2=centre_translated.eyeR.y-centre_face.eyeR.y;
 Dy3=centre_translated.nose.y-centre_face.nose.y;
-Dy4=centre_translated.mouth.y-centre_face.mouth.y;
-Dy=mean([Dy1,Dy2,Dy3,Dy4]);
+%Dy4=centre_translated.mouth.y-centre_face.mouth.y;
+%Dy=mean([Dy1,Dy2,Dy3,Dy4]);
 
-translation = [1 0 Dx; 0 1 Dy; 0 0 1];
+translation = [1 0 Dx3; 0 1 Dy3; 0 0 1];
 
 %alanwashere
