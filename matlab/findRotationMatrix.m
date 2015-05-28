8a1b79a16d91c449c7c3854791de172e883f6245
alanxwl <alanxwl@live.com>
2015-05-28 19:09:47 +0900

    Update findRotationMatrix.m

--- matlab/findRotationMatrix.m
+++ matlab/findRotationMatrix.m
@@ -8,19 +8,24 @@ function rotation = findRotationMatrix(centre_face, centre_rotated)
 
 %offset eyeL (original) and eyeL1 (rotated); nose as origin; express as x y coords
 eyeL = [centre_face.eyeL.x - centre_face.nose.x, centre_face.eyeL.y - centre_face.nose.y];
-eyeL1 = [centre_rotated.eyeL.x - centre_face.nose.x, centre_rotated.eyeL.y - centre_face.nose.y];
+eyeL1 = [centre_rotated.eyeL.x - centre_rotated.nose.x, centre_rotated.eyeL.y - centre_rotated.nose.y];
 
-%find angle between eyeL (original) and eyeL1 (rotated)
-costheta = (dot(eyeL, eyeL1))/((norm(eyeL))*(norm(eyeL1)));
-theta = acos(costheta);
+angleface = atan(eyeL(2)/eyeL(1));
+anglerotated = atan(eyeL1(2)/eyeL(1));
+theta=anglerotated-angleface;
 
-%check whether angle (theta) is negative
-xaxis = [1,0];
-rho = acos((dot(eyeL, xaxis))/((norm(eyeL))*(norm(xaxis))));
-rho1 = acos((dot(eyeL1, xaxis))/((norm(eyeL1))*(norm(xaxis))));
-if (rho1<rho)
-    theta = -theta;
-end
+
+% %find angle between eyeL (original) and eyeL1 (rotated)
+% costheta = (dot(eyeL, eyeL1))/((norm(eyeL))*(norm(eyeL1)));
+% theta = acos(abs(costheta));
+% 
+% %check whether angle (theta) is negative
+% xaxis = [-1,0];
+% rho = acos(abs((dot(eyeL, xaxis))/((norm(eyeL))*(norm(xaxis)))));
+% rho1 = acos(abs((dot(eyeL1, xaxis))/((norm(eyeL1))*(norm(xaxis)))));
+% if (rho1<rho)
+%     theta = -theta;
+% end
 
 %build rotation matrix, where [x'; y'; 1]=[rotation]*[x; y; 1]
 rotation = [cos(theta), -sin(theta), 0; sin(theta), cos(theta), 0; 0, 0, 1];
