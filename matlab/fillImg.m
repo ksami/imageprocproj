3ff1906ae54d914f0393ad30d88fd8ee9ac3be08
alanxwl <alanxwl@live.com>
2015-05-29 19:55:45 +0900

    Update fillImg.m

--- matlab/fillImg.m
+++ matlab/fillImg.m
@@ -2,14 +2,53 @@ function filledImg = fillImg(img)
 % Fills in the gaps resulting from an image being scaled
 
 [row, col, dim] = size(img);
+filledImg=zeros(size(img));
+pizza=zeros(size(img));
 
 for d=1:dim
-    for y=1:row
-        for x=1:col
-            for dy=-1:1:1
-                for dx=-1:1:1
+    chicken=img(:,:,d);
+    chicken=padarray(chicken,[1 1]);
+    
+    for y=2:row+1
+        for x=2:col+1
+            sum=0;
+            count = 0;
+            
+            if (chicken(y,x)==0)
+                
+%                 sum=chicken(y-1,x-1)+chicken(y,x-1)+chicken(y+1,x-1)+chicken(y-1,x)+chicken(y+1,x)+chicken(y-1,x+1)+chicken(y,x+1)+chicken(y+1,x+1);
+%                
+%                 for y1=y-1:1:y+1
+%                     for x1=x-1:1:x+1
+%                         if (chicken(y1,x1)~=0)
+%                             count=count+1;
+%                         end
+%                     end
+%                 end
+                
+                
+                for dy=-1:1:1
+                    for dx=-1:1:1
+                        sum = sum + chicken(y+dy,x+dx);
+                        if (chicken(y+dy,x+dx)>0.1)
+                            count=count+1;
+                        end
+                    end
                 end
+                
+                if (count==0)
+                    pizza(y,x)=0;
+                else
+                    pizza(y,x)=sum/count;
+                end
+                
+            else
+                pizza(y,x)=chicken(y,x);
             end
+            
         end
     end
+    filledImg(:,:,d)=pizza(2:row+1,2:col+1);
+end
+filledImg=uint8(filledImg);
 end
